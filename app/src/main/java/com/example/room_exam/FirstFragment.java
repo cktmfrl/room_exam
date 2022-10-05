123a2d90bf6faeafdd6856411c997db42701bbaa
origin <sskkccha@gmail.com>
2022-10-05 23:52:39 +0900

    dataBinding 적용, 레이아웃 수정

---
+++ app/src/main/java/com/example/room_exam/FirstFragment.java
@@ -0,0 +1,47 @@
+package com.example.room_exam;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.room_exam.databinding.FragmentFirstBinding;
+
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
--- app/src/main/java/com/example/room_exam/MainActivity.java
+++ app/src/main/java/com/example/room_exam/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.room_exam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.room.Room;
 
 import android.os.Bundle;
@@ -8,31 +12,40 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.room_exam.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-    private EditText mTodoEditText;
-    private TextView mResuiltTextView;
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mTodoEditText = findViewById(R.id.todo_edit);
-        mResuiltTextView = findViewById(R.id.result_text);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
-                .allowMainThreadQueries()
-                .build();// 전역으로
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
 
-        mResuiltTextView.setText(db.todoDao().getAll().toString());
 
-        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-                mResuiltTextView.setText(db.todoDao().getAll().toString());// 메서드
-            }
-        });
+//        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, "todo-db")
+//                .allowMainThreadQueries()
+//                .build();// 전역으로
+//
+//
+//        mResuiltTextView.setText(db.todoDao().getAll().toString());
+//
+//        findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
+//                mResuiltTextView.setText(db.todoDao().getAll().toString());// 메서드
+//            }
+//        });
     }
 }
